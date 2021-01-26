2d893a35d2b9620003269a20e0833cb367e0dc82
Cole Fleming <cmfleming@eths202.org>
2021-01-26 21:39:44 +0000

    Added Iron Kit and Comments

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import wget
 import json
-import pprint
 import os
+#Create Constants and Global Variables
 global importedkits
 global nodelist
 global creatednodelist
-pp = pprint.PrettyPrinter(depth=4)
 creatednodelist=[]
 importedkits={}
 nodelist=[]
+#Associate Commands with Functions
 def commands():
   command=raw_input("")
   if command=="loadjson":
@@ -17,21 +17,30 @@ def commands():
     loadjson(True)
   elif command=="yml":
     loadjson(False)
+
+
+
 def loadjson(url):
   if url==True:
     #lnk=raw_input("Link to Json: ")
     lnk="https://api.npoint.io/18d115461b2ab1e22dce"
+    #Download JSON File from URL and put it in ./JSON/kitsimported.json
     wget.download(lnk, 'JSON/kitsimported.json')
   with open("./JSON/kitsimported.json", "r") as read_file:
     importedkits = json.load(read_file)
   createyml(nodelist,importedkits)
 def createnode(L,kit,ikk):
+  #Create Command Placeholder Value
   cmd="/k_"+ikk.get("kits").get(str(kit)).get("CMDID")
+  #Insert Placeholders into String
   finalnode="\'"+kit+"\'"+":\n  command: "+cmd+"\n  type: RUN_COMMAND\n  runcmd:"
+  #Iterate through commands and add them to final string
   for x in ikk.get("kits").get(str(kit)).get("INVSLOTS"):
     finalnode+="\n  - \'/replaceitem entity @s "+x.get("ID")+" "+x.get("ITEM")+" 1\'"
+  #Print Results Formatted in Unicode
   print(u'\n'+finalnode)
 def createyml(l,ik):
+  #Iterate through each kit
   for x in ik.get("kits"):
     l.append(createnode(creatednodelist,x,ik))
 commands()
